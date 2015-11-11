d80559cdedbe8d2999cb93c1f693b213618da7e7
rajatk32 <rajatk@andrew.cmu.edu>
2015-11-10 21:42:19 -0500

    added example of how to insert into db from spider in myspider.py

--- tutorial/spiders/myspider.py
+++ tutorial/spiders/myspider.py
@@ -1,4 +1,11 @@
 import scrapy
+
+import urllib
+import urllib2
+import json
+
+url = 'http://localhost:8888/crawlerdb.db/TestTable2/'
+
 from tutorial.items import myitem
 class myspider(scrapy.Spider):
     name = "myspider"
@@ -13,4 +20,17 @@ class myspider(scrapy.Spider):
             item['title'] = sel.xpath('a/text()').extract()
             item['link'] = sel.xpath('a/@href').extract()
             item['desc'] = sel.xpath('text()').extract()
+	    
+	    ##### This part is for inserting into DB #####
+	    title = item['title'][0] if item['title'] else None
+	    link = item['link'][0] if item['link'] else None
+	    desc = item['desc'][0] if item['desc'] else None
+	    params = urllib.urlencode({
+		'title' : title,
+		'link' : link,
+		'desc' : desc
+	    })
+	    response = urllib2.urlopen(url, params).read()
+	    ##### End of insertion into DB #####
+
             yield item

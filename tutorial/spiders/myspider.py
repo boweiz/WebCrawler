0f61d8cf2d02ef6416d972d763b03aab979c2f2c
BarryKeCMU <lieweik@andrew.cmu.edu>
2015-11-11 21:18:06 -0500

    Added the features of output status and pause for two seconds.

--- tutorial/spiders/myspider.py
+++ tutorial/spiders/myspider.py
@@ -1,26 +1,34 @@
 import scrapy
-
+import time
 import urllib
 import urllib2
 import json
+
+#Worked with Xi Chen, Bowei Zhang
 
 url = 'http://localhost:8888/crawlerdb.db/TestTable2/'
 
 from tutorial.items import myitem
 class myspider(scrapy.Spider):
     name = "myspider"
-    allowed_domains = ["http://www.cmu.edu/"]
+    allowed_domains = ["https://leetcode.com/"]
     start_urls = [
-        "http://www.cmu.edu/research/"
+        "https://leetcode.com/problemset/algorithms/"
 	]
 
     def parse(self, response):
-         for sel in response.xpath('//ul/li'):
+         for sel in response.xpath('//tr/td'):
             item = myitem()
             item['title'] = sel.xpath('a/text()').extract()
             item['link'] = sel.xpath('a/@href').extract()
             item['desc'] = sel.xpath('text()').extract()
-	    
+            
+            ## print and sleep 2 second
+            print "tilte = " , sel.xpath('a/text()').extract()
+            print "link = ", sel.xpath('a/@href').extract()
+            print "desc = ", sel.xpath('text()').extract()
+            time.sleep(2)
+            
 	    ##### This part is for inserting into DB #####
 	    title = item['title'][0] if item['title'] else None
 	    link = item['link'][0] if item['link'] else None
@@ -34,3 +42,5 @@ class myspider(scrapy.Spider):
 	    ##### End of insertion into DB #####
 
             yield item
+
+#ur/li
